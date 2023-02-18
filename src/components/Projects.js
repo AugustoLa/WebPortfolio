7575310ec9e216cc7b55fcc1dbbf01122d6245f8
AugustoLa <augusto_florindo@hotmail.com>
2023-02-18 02:18:43 -0300

    responsividade newsletter melhorada

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -42,10 +42,10 @@ export const Projects = () => {
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                    <Nav.Link eventKey="first">Finalizados</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                    <Nav.Link eventKey="second">Em andamento</Nav.Link>
                                 </Nav.Item>
                             </Nav>
                             <Tab.Content>
@@ -64,7 +64,7 @@ export const Projects = () => {
                                     </Row>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                    <p>Nada por enquanto</p>
                                 </Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
